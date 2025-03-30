4f79d870d53f67ad92453dca03931f5521acd245
ReverionTech <reveriontech@gmail.com>
2025-03-30 23:22:36 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
 
-  rollupOptions: {
-      external: ['react-router-dom'],
-    },
+
   optimizeDeps: {
     esbuildOptions: {
       define: {
